3a92b678746b132f106634129c28d8e1a705623a
Pär Thernström <par@earthpeople.se>
2015-10-12 22:24:27 +0200

    add cpu logger

---
+++ loggers/CPUUsageLogger.php
@@ -0,0 +1,47 @@
+<?php
+
+class CPUUsageLogger extends SimpleLogger {
+
+    public $slug = __CLASS__;
+
+    function getInfo() {
+
+        $arr_info = array(
+            "name" => "CPU Usage Logger",
+            "description" => "Logs CPU usage",
+            "capability" => "manage_options",
+            "messages" => array(),
+            "labels" => array(),
+        );
+
+        return $arr_info;
+
+    }
+
+    function loaded() {
+
+        add_action("init", array( $this, "log_usage" ), 10, 2);
+
+    }
+
+    function log_usage() {
+
+        // 1, 5, 15
+        $load = sys_getloadavg();
+
+        SimpleLogger()->debug(
+            '
+            CPU usage last 1 minute: {cpu_1_minutes},
+            CPU usage last 5 minutes: {cpu_5_minutes},
+            CPU usage last 15 minutes: {cpu_15_minutes},
+            ',
+            array(
+                "cpu_1_minutes" => round( $load[0], 4 ),
+                "cpu_5_minutes" => round( $load[1], 4),
+                "cpu_15_minutes" => round( $load[2], 4),
+            )
+        );
+
+    }
+
+}
