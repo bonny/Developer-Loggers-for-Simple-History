d9b8af9ea8dee7ca4aa83b4526c0958211dafefe
Pär Thernström <par@earthpeople.se>
2015-10-26 20:28:11 +0100

    show loggers in i table. much better overview.

--- templates/settings.php
+++ templates/settings.php
@@ -3,35 +3,60 @@
     <!-- <h3>Loggers</h3> -->
     <form method="post">
 
-        <ul>
-            <?php
+        <?php
 
-            $available_loggers = $this->get_available_loggers();
+        $available_loggers = $this->get_available_loggers();
+
+        if ( $available_loggers ) {
+
+            printf('
+                <table class="widefat wp-list-table plugins">
+                    <thead>
+                        <tr>
+                            <th>%3$s</th>
+                            <th>%1$s</th>
+                            <th>%2$s</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                ',
+                __("Name", "simple-history"),
+                __("Description", "simple-history"),
+                __("Enabled", "simple-history")
+            );
 
             foreach ( $available_loggers as $logger ) {
 
+                $is_enabled = $this->is_logger_enabled( $logger["slug"] );
+
                 printf('
-                    <li>
-                        <h4>%1$s</h4>
-                        <p>%2$s</p>
-                        <p>
+                    <tr class="%5$s">
+                        <td>
                             <label>
                                 <input type="checkbox" %4$s name="enabled_loggers[]" value="%3$s">
-                                Enable
                             </label>
-                        </p>
-                    </li>
+                        </td>
+                        <td>%1$s</td>
+                        <td>%2$s</td>
+                    </tr>
                     ',
                     $logger["info"]["name"], // 1
                     $logger["info"]["description"], // 2
                     $logger["slug"] , // 3
-                    checked(true, $this->is_logger_enabled( $logger["slug"] ), false) // 4
+                    checked(true, $is_enabled, false), // 4
+                    $is_enabled ? "active" : "inactive"
                 );
 
-            }
+            } // for each logger
 
-            ?>
-        </ul>
+            printf('
+                </tbody>
+                </table>
+            ');
+
+        } // if available loggers
+        ?>
+
 
         <input type="hidden" name="<?php echo $this->slug ?>_action" value="save_settings">
         <?php
