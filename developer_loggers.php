ca39a317373c435a19b9a8c0b29c329a34a01fd3
Pär Thernström <par.thernstrom@gmail.com>
2018-05-20 21:40:29 +0200

    Convert indentation to tabs + misc cleanup

--- developer_loggers.php
+++ developer_loggers.php
@@ -8,66 +8,64 @@
  * Author: Pär Thernström
  */
 
-if ( version_compare( phpversion(), "5.4", ">=") ) {
+if ( version_compare( phpversion(), '5.4', '>=' ) ) {
 
-    // ok
-    include_once( __DIR__ . "/class.php" );
+	include_once( __DIR__ . '/class.php' );
 
-    /**
-     * Init plugin using the add_custom_logger filter
-     */
-    function SimpleHistory_DeveloperLoggers_addCustomLogger( $simpleHistory ) {
+	/**
+	 * Init plugin using the add_custom_logger filter
+	 *
+	 * @param Simple History instance $simpleHistory Simple history instance.
+	 */
+	function SimpleHistory_DeveloperLoggers_addCustomLogger( $simpleHistory ) {
 
-        $logger = new SimpleHistory_DeveloperLoggers;
-        $logger->init( $simpleHistory );
+		$logger = new SimpleHistory_DeveloperLoggers;
+		$logger->init( $simpleHistory );
 
-    }
+	}
 
-    add_action( "simple_history/add_custom_logger", "SimpleHistory_DeveloperLoggers_addCustomLogger" );
+	add_action( "simple_history/add_custom_logger", "SimpleHistory_DeveloperLoggers_addCustomLogger" );
 
-    /**
-     * Fallback if Simple History is not installed
-     * Show message about it
-     */
-    add_action( "admin_init", function() {
+	/**
+	 * Fallback if Simple History is not installed
+	 * Show message about it
+	 */
+	add_action( 'admin_init', function() {
 
-        if ( ! is_plugin_active("simple-history/index.php") ) {
+		if ( ! class_exists( 'SimpleHistory' ) ) {
 
-            add_action( 'admin_notices', function() {
+			add_action( 'admin_notices', function() {
+				?>
+				<div class="updated error">
+					<p><?php _e( '"Developer Loggers for Simple History" requires that the plugin "Simple History" is installed and activated.', 'simple-history' ); ?></p>
+				</div>
+				<?php
+			} );
 
-                ?>
-                <div class="updated error">
-                    <p><?php _e( '"Developer Loggers for Simple History" requires that the plugin "Simple History" is installed and activated.', 'simple-history' ); ?></p>
-                </div>
-                <?php
+		}
 
-            } );
-
-        }
-
-    } );
+	} );
 
 } else {
 
-    // not ok
-    // user is running to old version of php, add admin notice about that
-    add_action( 'admin_notices', 'SimpleHistory_DeveloperLoggers_oldVersionNotice' );
+	// User is running to old version of php, add admin notice about that.
+	add_action( 'admin_notices', 'simple_history_developer_loggers_old_version_notice' );
 
-    function SimpleHistory_DeveloperLoggers_oldVersionNotice() {
-        ?>
-        <div class="updated error">
-            <p><?php
-
-                printf(
-                    __( 'Developer Loggers for Simple History requires at least PHP 5.4 (you have version %s).', 'simple-history' ),
-                    phpversion()
-                );
-
-                ?></p>
-        </div>
-        <?php
-
-    }
-
-
+	/**
+	 * Show admin notice about old php version.
+	 */
+	function simple_history_developer_loggers_old_version_notice() {
+		?>
+		<div class="updated error">
+			<p>
+				<?php
+				printf(
+					__( 'Developer Loggers for Simple History requires at least PHP 5.4 (you have version %s).', 'simple-history' ),
+					esc_html( phpversion() )
+				);
+				?>
+			</p>
+		</div>
+		<?php
+	}
 }
